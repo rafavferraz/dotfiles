51386c6164c8fde2885d703610ee3db7adf36c36
Rafael Ferraz <rafavferraz@gmail.com>
2026-03-23 04:58:51 -0300

    zed update

--- scripts/packages/clang.sh
+++ scripts/packages/clang.sh
@@ -2,8 +2,21 @@
 
 if command -v clang++ &>/dev/null; then
     echo "clang++ already installed"
-    exit 0
+else
+    echo "=== Installing clang++ ==="
+    sudo apt install -y clang
 fi
 
-echo "=== Installing clang++ ==="
-sudo apt install -y clang
+if command -v clang-format-20 &>/dev/null; then
+    echo "clang-format-20 already installed"
+else
+    echo "=== Installing clang-format-20 ==="
+    sudo apt install -y clang-format-20
+fi
+
+if command -v clang-tidy-20 &>/dev/null; then
+    echo "clang-tidy-20 already installed"
+else
+    echo "=== Installing clang-tidy-20 ==="
+    sudo apt install -y clang-tidy-20
+fi
